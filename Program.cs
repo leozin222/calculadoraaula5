cbcfeb194d0ce1e44616e4e9b4d1c7463b3082e9
leozin222 <104734167+leozin222@users.noreply.github.com>
2024-08-27 21:39:42 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Calculadora calc = new Calculadora();
+
+Console.WriteLine("Informe o valor de X: ");
+
+double x = Convert.ToDouble (Console.ReadLine());
+
+Console.WriteLine("Informe o Valor de Y: ");
+
+double y = Convert.ToDouble (Console.ReadLine());
+
+double resultadoSoma = calc.Soma(x,y);
+
+Console.WriteLine($"A soma de {x} e {y} é igual á: {resultadoSoma}");
+
+
+Console.WriteLine($"O resultado da subtração é {calc.Subtracao(x,y)}");
+
+Console.WriteLine($"O resultado da Divisão é {calc.Divisao(x,y)}");
+
+Console.WriteLine($"O resultado da Multiplcação é {calc.Multiplicacao(x,y)}");
