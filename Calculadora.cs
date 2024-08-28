14acd0a59bb741e2485c4c9a137018b80d5dc1eb
leozin222 <104734167+leozin222@users.noreply.github.com>
2024-08-27 21:41:53 -0300

    Add files via upload

---
+++ Calculadora.cs
@@ -0,0 +1,20 @@
+class Calculadora
+{
+    public double Soma(double x, double y)
+    {
+        double resultado = x + y;
+        return resultado;
+    }
+    public double Subtracao(double x, double y)
+    {
+        return x - y;
+    }
+    public double Multiplicar(double x, double y)
+    {
+        return x * y;
+    }
+    public double Divisao(double x, double y)
+    {
+        return x / y;
+    }
+}
